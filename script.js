6af7b2a78b6c521b85347b1aea9e0c0df3f588a1
SAIFMOHD5542 <SAIFLEGION5542@OUTLOOK.COM>
2024-10-30 14:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('bookingForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Booking submitted!');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent!');
+});
